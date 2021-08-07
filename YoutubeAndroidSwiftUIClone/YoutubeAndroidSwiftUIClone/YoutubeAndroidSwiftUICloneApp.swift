d11b958acc15d3f52533211bca29048189fd0246
Daniel Vaswani <vaswanidaniel@gmail.com>
2021-08-07 08:07:02 +0700

    project init

---
+++ YoutubeAndroidSwiftUIClone/YoutubeAndroidSwiftUIClone/YoutubeAndroidSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAndroidSwiftUICloneApp.swift
+//  YoutubeAndroidSwiftUIClone
+//
+//  Created by Daniel Vaswani on 07/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAndroidSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
